0bfc2f460b1926c4c82c831d53d487334c1add3d
lidia123-web <lidiaheloisa123@gmail.com>
2026-05-05 14:35:34 +0000

    teste14.c

---
+++ teste13.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a = -10;
+    if (!a){//(!(a>0)
+        printf("A variável a é negativa.\n");
+    //printf("a não é um número positivo\n");
+    } else{
+        printf(" A variável é positiva.\n");
+    }
+}
