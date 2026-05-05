a2f18a5db18a389e9f0c9de0b8c8abfd203b257d
lidia123-web <lidiaheloisa123@gmail.com>
2026-05-05 14:21:15 +0000

    criei arquivo teste12.c

---
+++ teste12.c
@@ -0,0 +1,10 @@
+int main(){
+    int a= -10, b= -5;
+
+    if(a > 0 || b > 0)
+    {
+        printf("Os dois numeros são possitivos\n");
+    } else{
+        printf("Pelo menos um é negativo\n");
+    }
+}
