d645ed9ccfd0041ea135fa13d3dc1343ec17bb74
lidia123-web <lidiaheloisa123@gmail.com>
2026-05-10 18:11:47 +0000

    criei o arequivo teste23.c

---
+++ teste23.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main() {
+    int num1 = 25, num2 = 20;
+    int maior;
+
+    maior = (num1 > num2) ? num1 : num2;
+
+    printf("O maior numero é: %d\n", maior);
+    return 0;
+}
